9d21802e8ea5d0dcb83d1090c32509fca1d504df
chikhcode <chikhwriter@gmail.com>
2024-06-05 15:48:14 +0100

    Add files via upload

---
+++ serie-2/script4.js
@@ -0,0 +1,23 @@
+function checkAnswer() {
+    const correctAnswer = "Madrid";
+    const selectedOption = document.querySelector('input[name="answer"]:checked');
+    const result = document.getElementById('result');
+
+    if (selectedOption) {
+        if (selectedOption.value === correctAnswer) {
+            result.textContent = "Bien joué, continuez comme ça !";
+            result.style.color = "green";
+        } else {
+            result.textContent = `Incorrecte ! La bonne réponse est ${correctAnswer}. Continuez, c’est par ses erreurs qu’on s’améliore.`;
+            result.style.color = "red";
+        }
+        document.getElementById('nextExercise').style.display = 'block';
+    } else {
+        result.textContent = "Veuillez sélectionner une réponse.";
+        result.style.color = "orange";
+    }
+}
+
+function nextExercise() {
+    window.location.href = 'exercice5.html';
+}
